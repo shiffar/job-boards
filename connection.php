817d1270dbf557eaf4bd335caf3c93085b78180d
shiffar <109830061+shiffar@users.noreply.github.com>
2024-05-30 20:43:51 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection parameters
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "workman";
+$host = "sql101.infinityfree.com";
+$username = "if0_36645368";
+$password = "7jVMpQtrgm";
+$database = "if0_36645368_workman";
 
 // Create a database connection
 $conn = new mysqli($host, $username, $password, $database);
